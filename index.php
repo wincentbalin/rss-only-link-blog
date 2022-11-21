b7ea5bc49d2d6c288671a376ceaad8ceec009e4f
Wincent Balin <wincent.balin@gmail.com>
2022-11-21 23:38:36 +0100

    Moved index file output to a function

--- index.php
+++ index.php
@@ -1,15 +1,29 @@
 <?php
-$last_modified_format = 'D, d M Y H:i:s \\G\\M\\T';
-$stylesheet_line = <<<EOT
+
+function output_index($filename, $headers_only = FALSE)
+{
+    $stylesheet_line = <<<EOT
 <?xml-stylesheet type="text/xsl" href="tohtml5.xsl"?>
 
 EOT;
-header('Content-Type: application/xml');
-header('Content-Length: ' . filesize('index.xml') + strlen($stylesheet_line));
-header('Last-Modified: ' . date($last_modified_format, filemtime('index.xml')));
-$index_file = fopen('index.xml', 'r');
-echo fgets($index_file);  # Output declaration line
-echo $stylesheet_line;    # Output stylesheet line
-fpassthru($index_file);   # Output the rest of the index file and close it
-fclose($index_file);
+    $content_length = filesize($filename) + strlen($stylesheet_line);
+    $last_modified_format = 'D, d M Y H:i:s \\G\\M\\T';
+    $last_modified = date($last_modified_format, filemtime($filename));
+
+    header('Content-Type: application/xml');
+    header('Content-Length: ' . $content_length);
+    header('Last-Modified: ' . $last_modified);
+
+    if ($headers_only === TRUE)
+        exit;
+
+    $index_file = fopen($filename, 'r');
+    echo fgets($index_file);  # Output declaration line
+    echo $stylesheet_line;    # Output stylesheet line
+    fpassthru($index_file);   # Output the rest of the index file and close it
+    fclose($index_file);
+    exit;
+}
+
+output_index('index.xml');
 
