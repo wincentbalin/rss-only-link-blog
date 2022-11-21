3446b87d6d60f6a88305278e6b349b82b6ee9bcc
Wincent Balin <wincent.balin@gmail.com>
2022-11-21 23:27:07 +0100

    Implemented stylesheet insertion

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-echo "Hello, world!";
+$last_modified_format = 'D, d M Y H:i:s \\G\\M\\T';
+$stylesheet_line = <<<EOT
+<?xml-stylesheet type="text/xsl" href="tohtml5.xsl"?>
+
+EOT;
+header('Content-Type: application/xml');
+header('Content-Length: ' . filesize('index.xml') + strlen($stylesheet_line));
+header('Last-Modified: ' . date($last_modified_format, filemtime('index.xml')));
+$index_file = fopen('index.xml', 'r');
+echo fgets($index_file);  # Output declaration line
+echo $stylesheet_line;    # Output stylesheet line
+fpassthru($index_file);   # Output the rest of the index file and close it
+fclose($index_file);
 
